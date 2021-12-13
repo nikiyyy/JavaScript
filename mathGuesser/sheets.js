a07fa691f22b5c46825ed3abb6d208053e600b2f
nikiyyy <wow_niko@protonmail.com>
2021-12-13 20:47:15 +0200

    mathguresser
    almost ready

--- mathGuesser/sheets.js
+++ mathGuesser/sheets.js
@@ -1,20 +1,62 @@
-//document.getElementById("menu").style.display = "none";
 //style
 function menu()
 {
-
-    if (document.getElementById("menu").style.display == "none"){
-        console.log("eyrteyt");
-        document.getElementById("menu").style.display = "inline";
+    if (document.getElementById("options").style.display == "none"){
+        document.getElementById("options").style.display = "inline";
     }
     else
     { 
-        document.getElementById("menu").style.display = "none";
+        document.getElementById("options").style.display = "none";
     }
 }
 
-//functionality
-function getTask()
-{
-    console.log(Math.floor((Math.random(5))*5));
+function start(){
+
+    setInterval(function countdown()
+    {
+        if(document.getElementById("countdown").innerHTML > 0)
+        {
+            document.getElementById("countdown").innerHTML = parseInt(document.getElementById("countdown").innerHTML) - 1;
+        }
+        
+    }
+    ,1000);
+    generateTask();
 }
+//functionality 
+function generateTask()
+{
+    let a = Math.ceil((Math.random(9))*9);
+    let b = Math.ceil((Math.random(9))*9);
+
+    rightanswer = a+b;
+    document.getElementById("task").innerHTML = a.toString() + "+" + b.toString();
+
+
+}
+
+function inputAnswer(){
+    if(document.getElementById("countdown").innerHTML != 0){
+    if (document.getElementById("inputtext").value == rightanswer)
+    {
+        document.getElementById("score").innerHTML = parseInt(document.getElementById("score").innerHTML) + 1;
+    }
+    else
+    {
+        document.getElementById("wrong").innerHTML = parseInt(document.getElementById("wrong").innerHTML) + 1;
+    }
+    generateTask()
+    document.getElementById("inputtext").value = "";
+    }
+}
+
+function setTime(){
+    document.getElementById("countdown").innerHTML = document.getElementById("time").value ;
+}
+var rightanswer;
+
+document.getElementById("options").style.display = "none";
+
+document.getElementById("start").addEventListener("click", start);
+document.getElementById("answer").addEventListener("click", inputAnswer);
+document.getElementById("settime").addEventListener("click", setTime);
