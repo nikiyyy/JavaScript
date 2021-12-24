5ea8d8bd755c1698b0ae0e4b7333e2ebfc6af200
nikiyyy <wow_niko@protonmail.com>
2021-12-25 01:47:45 +0200

    v2

--- Quiz/quiz.js
+++ Quiz/quiz.js
@@ -1,1 +1,25 @@
-console.log("test");
+function fileRead(){
+    console.log("1");
+}
+
+function nextQuestion(){
+    console.log("2");
+}
+
+function prevQuestion(){
+    console.log("3");
+}
+
+document.getElementById('file-selector')
+.addEventListener('change', function() {
+  
+var fr=new FileReader();
+fr.onload=function(){
+    console.log(fr.result);
+}
+  
+fr.readAsText(this.files[0]);
+})
+
+document.getElementById("prev").addEventListener("click", fileRead );
+document.getElementById("next").addEventListener("click", fileRead );
