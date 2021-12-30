61ae412cb7c0e2687d8b87a4be85d8d8f2c26a19
nikiyyy <wow_niko@protonmail.com>
2021-12-30 02:33:46 +0200

    v1

--- password generator/generator.js
+++ password generator/generator.js
@@ -1,5 +1,31 @@
 function test(){
-    console.log((Math.random()).toString(36))
+    var string = "";
+    while(string.length < document.getElementById("PassLength").value){
+        var randomNumb = Math.floor(Math.random() * 4)
+        if( randomNumb == "0" && document.getElementById("LowerLetters").checked){
+            string += String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+        }
+        else if( randomNumb == "1" && document.getElementById("CapLetters").checked){
+            string += String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+        }
+        else if(randomNumb == "2" && document.getElementById("Numbers").checked){
+            string += String.fromCharCode(Math.floor(Math.random() * 10) + 46)
+        }
+        else if(randomNumb == "3" && document.getElementById("Symbols").checked){
+            const symbols = "!@#$%^&*()_+{}/.,"
+            string += symbols[Math.floor(Math.random() * symbols.length)]
+        }
+    }
+    console.log(string)
+    /*
+    //console.log((Math.random()).toString(36).slice(2, parseInt(document.getElementById("PassLength").value)+2));
+    console.log(String.fromCharCode(Math.floor(Math.random() * 26) + 97));
+    console.log(String.fromCharCode(Math.floor(Math.random() * 26) + 65));
+    console.log(String.fromCharCode(Math.floor(Math.random() * 10) + 46));
+    const symbols = "!@#$%^&*()_+{}/.,"
+    console.log(symbols[Math.floor(Math.random() * symbols.length)]);
+    */
+
     if(document.getElementById("CapLetters").checked){
         console.log("1")
     }
